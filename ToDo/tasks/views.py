7a1a914e32df6a9589bd8b9ee210f30dafb3ef88
EddyPBR <EdvaldoJunior1310@gmail.com>
2019-06-28 19:51:14 -0300

    adicionado as tabelas ao banco de dados

--- ToDo/tasks/views.py
+++ ToDo/tasks/views.py
@@ -8,4 +8,4 @@ def taskList(request):
     return render(request, 'tasks/list.html')
 
 def yourName(request, name):
-    return render(request, 'tasks/yourname.html', {'name': name})
+    return render(request, 'tasks/yourname.html', {'name':name})
