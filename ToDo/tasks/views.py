5a48938045aee9d25d45a81bb24320c25b9134cb
EddyPBR <EdvaldoJunior1310@gmail.com>
2019-06-29 13:22:18 -0300

    adicionado funçao de deletar e editar tarefas

--- ToDo/tasks/views.py
+++ ToDo/tasks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Task
 from .forms import TaskForm
+from django.contrib import messages
 
 def taskList(request):
     tasks = Task.objects.all().order_by('-created_at')
@@ -14,7 +15,6 @@ def taskView(request, id):
 def newTask(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
-
         if form.is_valid():
             task = form.save(commit=False)
             task.done = 'doing'
@@ -29,3 +29,26 @@ def helloWorld(request):
 
 def yourName(request, name):
     return render(request, 'tasks/yourname.html', {'name':name})
+
+def editTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    form = TaskForm(instance=task)
+
+    if(request.method == 'POST'):
+        form = TaskForm(request.POST, instance=task)
+
+        if(form.is_valid()):
+            task.save()
+            return redirect('/')
+        else:
+            return render(request, 'tasks/edittask.html', {'form':form, 'task': task })
+            
+    else:
+        return render(request, 'tasks/edittask.html', {'form':form, 'task': task })
+
+def deleteTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    task.delete()
+
+    messages.info(request, 'Tarefa deleteda com sucesso.')
+    return redirect('/')
